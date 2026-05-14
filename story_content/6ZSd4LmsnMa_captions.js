983351557aaf437438c3339858aa74c3f1f5a0f5
ConceptualAnitaBalik <anitabalik@gmail.com>
2026-05-14 14:48:45 +0200

    Add files via upload

---
+++ story_content/6ZSd4LmsnMa_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.98.34263.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:04.197%0D%0AThe%20sales%20representative%20receives%20a%20notification%20about%20the%20new%20hot%20lead.%0D%0A%0D%0A"}')
